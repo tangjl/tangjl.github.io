27bd9ad4fa8d6f794f45b19713801eab409faf51
tangjl <jordanltang@gmail.com>
2017-01-23 22:24:06 -0500

    workaround for the eye range

--- functions.js
+++ functions.js
@@ -22,17 +22,24 @@ jQuery(function($) {
         var width = Number($(window).width());
         var height = Number($(window).height());
 
-        if(currentMousePos.x <= 2000 && currentMousePos.y <= 1000) {
-            var left_pos_l = ( ( currentMousePos.x / (width * 2) ) * 55 ) + 10;
-            var right_pos_r =  (( currentMousePos.x / (width * 2)  ) * 55) + 22;
-            var top_pos_l = ( currentMousePos.y / (height) ) * 40 + 8;
-            var top_pos_r = ( currentMousePos.y / (height) ) * 40 + 8;
-            //and these
-            $('#location2').html('x: ' + left_pos_l + " , " + ' y: ' + top_pos_l)
-            $('#location3').html('x: ' + right_pos_r + " , " + ' y: ' + top_pos_r)
-                    
-            $('.eye-l').css('left',left_pos_l).css('top', top_pos_l);
-            $('.eye-r').css('left',right_pos_r).css('top',top_pos_r);
+        var left_pos_l = ( ( currentMousePos.x / (width * 2) ) * 55 ) + 8;
+        var right_pos_r =  (( currentMousePos.x / (width * 2)  ) * 55) + 20;
+        var top_pos_l = ( currentMousePos.y / (height) ) * 40 + 8;
+        var top_pos_r = ( currentMousePos.y / (height) ) * 40 + 8;
+        //and these
+        $('#location2').html('x: ' + left_pos_l + " , " + ' y: ' + top_pos_l)
+        $('#location3').html('x: ' + right_pos_r + " , " + ' y: ' + top_pos_r)
+
+        if(top_pos_l <= 48 && top_pos_r <= 48) {
+            if(Math.pow(left_pos_l - 32, 2) + Math.pow(top_pos_l - 32, 2) < 1024 &&
+            Math.pow(right_pos_r - 32, 2) + Math.pow(top_pos_r - 32, 2) < 1024) {
+                $('.eye-l').css('left',left_pos_l).css('top', top_pos_l);
+                $('.eye-r').css('left',right_pos_r).css('top',top_pos_r);
+            }
+        }
+        else {
+            $('.eye-l').css('left',23).css('top',27);
+            $('.eye-r').css('left',35).css('top',27);
         }
     }); 
 });
